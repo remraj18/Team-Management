02b0bc5ce1698805cc8fb221cf144f5fa81f8166
Prem kumar <160206017+remraj18@users.noreply.github.com>
2025-01-17 14:54:42 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
-import Header from '../components/Header';  // Relative path se import
-import Footer from '../components/Footer';  // Relative path se import
-import '../globals.css';  // Relative path se import
+
+import Header from '@/app/components/Header';  
+import Footer from '@/app/components/Footer';
+import '@/app/globals.css';
 
 export const metadata = {
   title: 'Team Dashboard',
@@ -11,12 +12,12 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <html>
       <body>
-        <div className="flex flex-col min-h-screen">
-          <Header />  {/* Header ko render kar rahe hain */}
-          <main className="flex-grow container mx-auto p-4">{children}</main>  {/* Children components ko render kar rahe hain */}
-          <Footer />  {/* Footer ko render kar rahe hain */}
-        </div>
-      </body>
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-grow container mx-auto p-4">{children}</main>
+      <Footer />
+    </div>
+    </body>
     </html>
   );
 };
